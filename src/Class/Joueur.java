eb8e1363f1853c512973b46050ad5119f067802e
CachalatXavier <xavier.cachalat@gmail.com>
2017-01-24 11:28:47 +0100

    classe joueur et position

--- src/Class/Joueur.java
+++ src/Class/Joueur.java
@@ -1,7 +1,39 @@
 package Class;
 
 public class Joueur {
-	int vie;
+	private int vie;
+	private String nom ; 
+	private Position pos;
+	
+	public Joueur( String nom){
+		this.nom= nom; 
+		this.pos = new Position(1,1);
+		this.vie = 10; 
+	}
+
+	public int getVie() {
+		return vie;
+	}
+
+	public void setVie(int vie) {
+		this.vie = vie;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Position getPos() {
+		return pos;
+	}
+
+	public void setPos(Position pos) {
+		this.pos = pos;
+	}
 	
 }
 

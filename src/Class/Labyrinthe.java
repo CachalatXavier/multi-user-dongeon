d2b0827faab08a054cf8bb44b1397cbd1369f5d7
Aldraeron <ormiere.corentin@gmail.com>
2017-01-26 15:27:13 +0100

    Creation Class Labyrinthe +Piece 1&2

---
+++ src/Class/Labyrinthe.java
@@ -0,0 +1,23 @@
+package Class;
+
+import java.util.ArrayList;
+
+public class Labyrinthe {
+	private ArrayList<Piece> Donjon = new ArrayList<Piece>(); 
+	
+	void CreationLabyrinthe(){
+		Position p1 =new Position (3,1);
+		Piece piece1= new Piece(p1,0);
+		Donjon.add(piece1);
+		
+		Position p2 =new Position (3,1);
+		Piece piece2= new Piece(p2,0);
+		Donjon.add(piece2);
+		
+		Porte p1_2 = new Porte(piece1,piece2);
+		
+		
+		
+	}
+
+}

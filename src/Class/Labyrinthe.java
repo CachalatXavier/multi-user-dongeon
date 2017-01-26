360e0fd03911adb28c9887dc439e3952b1c4565c
Spada66 <ca.meyer1996@gmail.com>
2017-01-26 16:15:23 +0100

    Serveur creation perso + labyrinthe

--- src/Class/Labyrinthe.java
+++ src/Class/Labyrinthe.java
@@ -3,19 +3,13 @@ package Class;
 import java.util.ArrayList;
 
 public class Labyrinthe {
-	private ArrayList<Piece> Donjon = new ArrayList<Piece>(); 
+	public ArrayList<Piece> Donjon = new ArrayList<Piece>(); 
 	
-	void CreationLabyrinthe(){
-		Position p1 =new Position (3,0);
-		Piece piece1= new Piece(p1,0);
-		Donjon.add(piece1);
-		
-		Position p2 =new Position (3,1);
-		Piece piece2= new Piece(p2,0);
-		Donjon.add(piece2);
-		
-		Porte p1_2 = new Porte(piece1,piece2);
-		
+	
+	
+	public Labyrinthe(ArrayList<Piece> donjon) {
+		super();
+		Donjon = donjon;
 	}
 
 }
--- src/Class/Serveur.java
+++ src/Class/Serveur.java
@@ -1,5 +1,6 @@
 package Class;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Serveur {
@@ -33,30 +34,49 @@ public class Serveur {
 		  		+ "2) Quitter \n");
 		  Scanner menu = new Scanner(System.in);
 		  int i = menu.nextInt();
-		  switch (i)
-		  {
-		    case 1:
-		      //Creation Personnage
-		    		System.out.println("Bonjour comment s'apelle votre Personnage?");
-		    		Scanner perso = new Scanner(System.in);
-		  		  	String nom = perso.nextLine();
-		  		  	System.out.println("Votre Personnage s'apelle: "+nom);
-		  		  	Joueur joueur1 = new Joueur (nom);
-		  		  	
-		  		  	System.out.println("Vous entrez maintenant dans le Donjon! \n"
-		  		  			+ "Que la chance vous sourie aventurier....");
-		  		  	
-		  		  	joueur1.setPiece(piece1);
-
-		    break;
-		    case 2:
-			      //Quitter le programme
-			      break;
-		    default:
-		      /*Action*/;
+		  
+		  if (i == 1){
+		    	
+		      	Joueur joueur1 = creationPerso();			      	
+		      	Labyrinthe labyrinthe1 = CreationLabyrinthe();
+		      	joueur1.setPiece(labyrinthe1.Donjon.get(0));		      		 
 		  }
 		  
-		  
+	//	  else {
+			      //Quitter le programme			  
+		    
+		      /*Action*/;
+		//  }
+		  		  		  
 	}
 
+	public static Joueur creationPerso(){
+		//Creation Personnage
+		System.out.println("Bonjour comment s'appelle votre Personnage?");
+		Scanner perso = new Scanner(System.in);
+		  	String nom = perso.nextLine();
+		  	System.out.println("Votre Personnage s'appelle: "+nom);
+		  	
+		  	System.out.println("Vous entrez maintenant dans le Donjon! \n"
+		  			+ "Que la chance vous sourie aventurier....");
+		  	Joueur joueur1 = new Joueur (nom);
+		  	return joueur1;
+	}
+	
+	public static Labyrinthe CreationLabyrinthe(){
+		ArrayList Donjon = new ArrayList<Piece>(); 
+		Labyrinthe labyrinthe1 = new Labyrinthe(Donjon);
+		Position p1 =new Position (3,0);
+		Piece piece1= new Piece(p1,0);
+		labyrinthe1.Donjon.add(piece1);
+		
+		Position p2 =new Position (3,1);
+		Piece piece2= new Piece(p2,0);
+		labyrinthe1.Donjon.add(piece2);
+		
+		Porte p1_2 = new Porte(piece1,piece2);
+		return labyrinthe1;
+	}
+	
+	
 }

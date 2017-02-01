24c06e43642e04ed6507222ba9b4f8ef8f22ff49
Aldraeron <ormiere.corentin@gmail.com>
2017-02-01 15:06:23 +0100

    Fix labyrinthe et ajout piece 3 et 5

--- src/Class/Labyrinthe.java
+++ src/Class/Labyrinthe.java
@@ -41,22 +41,47 @@ public class Labyrinthe {
 		Labyrinthe labyrinthe1 = new Labyrinthe(Donjon);
 		
 		
-		
+		//Creation piece 1
 		Position p1 =new Position (3,0);
-		Piece piece1= new Piece(p1,0);
+		Piece piece1= new Piece(p1,1);
 		
+		//Creation piece 2
 		Position p2 =new Position (3,1);
-		Piece piece2= new Piece(p2,1);
-		
-		Position p3 =new Position (4,0);
-		Piece piece3= new Piece(p3,2);
-		Porte p2_3 = new Porte(piece1,"E",piece3,"O");
+		Piece piece2= new Piece(p2,2);
+		//Creation porte piece1/piece2
 		Porte p1_2 = new Porte(piece1,"S",piece2,"N");
-	    piece1 =piece1.ajoutPorte(piece1, p1_2);
-		piece2 =piece1.ajoutPorte(piece2, p1_2);
+		//Ajout porte 
+		piece1 =piece1.ajoutPorte(piece1, p1_2);
+		piece2 =piece2.ajoutPorte(piece2, p1_2);
+		
+		
+		//Creation piece 3
+		Position p3 =new Position (4,1);
+		Piece piece3= new Piece(p3,3);
+		
+		//Creation porte piece2/piece3
+		Porte p2_3 = new Porte(piece2,"E",piece3,"O");
+		//Ajout Porte
+		piece3 =piece3.ajoutPorte(piece3, p2_3);
+		piece3 =piece3.ajoutPorte(piece2, p2_3);
+		
+		
+		//Creation piece 5
+		Position p5 =new Position (2,1);
+		Piece piece5= new Piece(p5,5);
+		
+		//Creation porte piece2/piece5
+		Porte p2_5 = new Porte(piece2,"E",piece5,"O");
+		//Ajout Porte
+		piece5 =piece5.ajoutPorte(piece5, p2_5);
+		piece5 =piece5.ajoutPorte(piece2, p2_5);
+		
+	   
 		
 		labyrinthe1.Donjon.add(piece1);
 		labyrinthe1.Donjon.add(piece2);
+		labyrinthe1.Donjon.add(piece3);
+		labyrinthe1.Donjon.add(piece5);
 		//Porte p1_2 = new Porte(piece1,piece2);
 		labyrinthe1.SearchPorte.add(p1_2);
 		

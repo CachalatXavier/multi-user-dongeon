04e64d4decb23d73721666d6a1bf390e19b8f57b
Aldraeron <ormiere.corentin@gmail.com>
2017-02-07 15:12:08 +0100

    Serveur Combat +Interface

---
+++ src/Class/ServeurCombat.java
@@ -0,0 +1,28 @@
+package Class;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+public class ServeurCombat extends UnicastRemoteObject implements interfaceObjetCombat {
+	
+	public ServeurCombat() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+	public static void main(String[] args)  throws Exception{
+		// TODO Auto-generated method stub
+
+		LocateRegistry.createRegistry(1101);
+		ServeurCombat obj = new ServeurCombat();
+		Naming.bind("ServeurDeCombat", obj);
+		System.out.println("Serveur de Combat déclarée");
+		
+		
+	}
+
+}

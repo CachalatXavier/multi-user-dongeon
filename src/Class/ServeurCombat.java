7adf3f753e2fa6e2a134e485930f553e06292eb1
Aldraeron <ormiere.corentin@gmail.com>
2017-02-08 11:29:53 +0100

    Debut combat

--- src/Class/ServeurCombat.java
+++ src/Class/ServeurCombat.java
@@ -30,5 +30,34 @@ public class ServeurCombat extends UnicastRemoteObject implements interfaceObjet
 		return i;
 	}
 	
+	public Vivant Combat(Piece p,Vivant v1,Vivant v2){
+		
+		//Passer la piece en combat
+		System.out.println("Vous entrez en combat");
+		while(1>0){
+			int random = 0 + (int)(Math.random() * ((100 - 0) + 1));//Compris entre 0 et 100
+				if (random>=50){
+				v2.setPdv(v2.getPdv()-1);
+				
+				}else{
+				v1.setPdv(v1.getPdv()-1);
+				}
+				if(v1.getPdv()== 0){
+				return v1;
+				}
+				if(v2.getPdv()== 0){
+				return v2;
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					//e.printStackTrace();
+				}
+		}
+
+	}
+	
+	
 
 }

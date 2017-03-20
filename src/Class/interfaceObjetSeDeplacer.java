62388a1ae329b5d8747688da1840c275c6ffe61d
CachalatXavier <xavier.cachalat@gmail.com>
2017-03-20 17:51:44 +0100

    oublie de 2 commentere

--- src/Class/interfaceObjetSeDeplacer.java
+++ src/Class/interfaceObjetSeDeplacer.java
@@ -7,14 +7,14 @@ import java.util.ArrayList;
 public interface interfaceObjetSeDeplacer extends Remote {
 	public ArrayList<Porte> porteDispo (Piece piece) throws RemoteException ;  
 	
-	public boolean DetectionMonstre(int ID)throws RemoteException;
-	public Piece getPiece1()throws RemoteException;
-	public Piece getPiece(int x) throws RemoteException;
+	public boolean DetectionMonstre(int ID)throws RemoteException; // renvoie si il y un monstre
+	public Piece getPiece1()throws RemoteException; // renvoie la piece1
+	public Piece getPiece(int x) throws RemoteException; // renvoie la piéce au joueur 
 	public int getSize() throws RemoteException;
-	public void retirerlistJoueur (Joueur joueur, Piece piece)throws RemoteException ; 
+	public void retirerlistJoueur (Joueur joueur, Piece piece)throws RemoteException ; // retire le joueur de la liste de la piece
 	public ArrayList<Monstre> renvoieDernierMonstre(Joueur J ) throws RemoteException ; 
-	public void ajoutlistJoueur(Joueur joueur, Piece piece) throws RemoteException;
-	public void miseAJourPosition(Joueur J, Piece nP, Piece oP) throws RemoteException;
+	public void ajoutlistJoueur(Joueur joueur, Piece piece) throws RemoteException; // ajoute un joueur dans la liste de la piece
+	public void miseAJourPosition(Joueur J, Piece nP, Piece oP) throws RemoteException; // met a jour la position du joueur
 	public void addClientListener (Alerte listener) throws java.rmi.RemoteException;	
 	public void retirerJoueurSalle (Joueur J)throws java.rmi.RemoteException;
 }
